dd6ac17e62fe64285884e8f7ef0777fcbd5d7cf1
Adeniyi Olanrewaju <adeniyiboladale@yahoo.com>
2023-02-04 02:14:50 +0000

    image upload

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,9 @@ from extensions import app
 
 # This function creates the Flask app and registers the Blueprint
 def create_app():
+    app.config["SECRET_KEY"] = "4f557e8e5eb51bfb7c42"
+    app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
     # Register the Blueprint
     app.register_blueprint(blp)
     # Return the app
--- routes/__init__.py
+++ routes/__init__.py
@@ -1,9 +1,25 @@
 from extensions import blp
-from flask import render_template
+from flask import render_template, redirect, request, url_for, flash
+import os
+import random
 
 
 # This is the route for the index page
-@blp.route('/')
+@blp.route('/', methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        if "fileInput" in request.files:
+            random_num = random.randint(0000, 9999)
+            image = request.files["fileInput"]
+            if image.filename != '':
+                image.save(os.path.join("static\\images", image.filename))
+                return redirect(url_for("routes.display", img=image.filename,
+                                        num=random_num))
+            flash("Nothing to upload", "danger")
     # Render the index.html template
     return render_template("index.html")
+
+
+@blp.route("/display/<img>/<num>")
+def display(img, num):
+    return render_template("display.html", img=img)
