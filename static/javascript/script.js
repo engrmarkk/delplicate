dd441b13dfcc3c6a924f92f85546273f91dd3f30
Adeniyi Olanrewaju <adeniyiboladale@yahoo.com>
2023-05-04 14:25:42 +0100

    javascript modified

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -2,3 +2,14 @@
 function chooseFile() {
   document.getElementById("fileInput").click();
 }
+
+
+
+
+// function chooseFile() {
+//     document.getElementById("fileInput").click();
+//   }
+
+document.getElementById("fileInput").addEventListener("change", function() {
+      document.getElementById("upload-form").submit();
+});
